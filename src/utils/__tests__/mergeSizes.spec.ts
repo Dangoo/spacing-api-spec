6078c1c86f4de87560115c305374f1e55d526650
dangoo <daniel.gooss@sinnerschrader.com>
2020-02-16 15:31:23 +0100

    feat: refactor merge sizes API

--- src/utils/__tests__/mergeSizes.spec.ts
+++ src/utils/__tests__/mergeSizes.spec.ts
@@ -1,26 +1,32 @@
 import { mergeSizes } from "../mergeSizes";
 
 describe('mergeSizes', (): void => {
+    it('works with empty override array', () => {
+        const res = mergeSizes([1, 2, 3, 4], []);
+
+        expect(res).toStrictEqual([1, 2, 3, 4]);
+    });
+
     it('merges blockStart correctly', () => {
-        const res = mergeSizes([1, 2, 3, 4], 0);
+        const res = mergeSizes([1, 2, 3, 4], [0, , , ,]);
 
         expect(res).toStrictEqual([0, 2, 3, 4]);
     });
 
     it('merges inlineEnd correctly', () => {
-        const res = mergeSizes([1, 2, 3, 4], undefined, 0);
+        const res = mergeSizes([1, 2, 3, 4], [, 0, , ,]);
 
         expect(res).toStrictEqual([1, 0, 3, 4]);
     });
 
     it('merges blockEnd correctly', () => {
-        const res = mergeSizes([1, 2, 3, 4], undefined, undefined, 0);
+        const res = mergeSizes([1, 2, 3, 4], [, , 0, ,]);
 
         expect(res).toStrictEqual([1, 2, 0, 4]);
     });
 
     it('merges inlineStart correctly', () => {
-        const res = mergeSizes([1, 2, 3, 4], undefined, undefined, undefined, 0);
+        const res = mergeSizes([1, 2, 3, 4], [,,,0]);
 
         expect(res).toStrictEqual([1, 2, 3, 0]);
     });
--- src/utils/mergeSizes.ts
+++ src/utils/mergeSizes.ts
@@ -4,25 +4,25 @@ import { expand } from "./expand";
 /**
  * @template S
  * @param {S[]} shorthand List of up to four sizes defined clockwise starting with top
- * @param {S} [blockStart] Size for upper side
- * @param {S} [inlineEnd] Size for right side
- * @param {S} [blockEnd] Size for lower side
- * @param {S} [inlineStart] Size for left side
+ * @param {[S, S, S, S]} overrides List of four overrid sizes defined clockwise starting with top
  * @returns {S[]}
  */
 export function mergeSizes<S>(
   shorthand: [] | [S] | Tuple<S> | Triple<S> | Quadrupel<S> = [],
-  blockStart?: S,
-  inlineEnd?: S,
-  blockEnd?: S,
-  inlineStart?: S
+  overrides: [] | Quadrupel<S>
 ): Quadrupel<S | undefined> {
   const expanded = expand(shorthand);
+  const [
+    blockStart = expanded[0],
+    inlineEnd = expanded[1],
+    blockEnd = expanded[2],
+    inlineStart = expanded[3]
+  ] = overrides;
 
   return [
-    typeof blockStart !== 'undefined' ? blockStart : expanded[0],
-    typeof inlineEnd !== 'undefined' ? inlineEnd : expanded[1],
-    typeof blockEnd !== 'undefined' ? blockEnd : expanded[2],
-    typeof inlineStart !== 'undefined' ? inlineStart : expanded[3],
+    blockStart,
+    inlineEnd,
+    blockEnd,
+    inlineStart
   ];
 }
